618316d438630e5628f6c69ca388096425023a90
Zipho Mashologu <zipho@sanbi.ac.za>
2020-07-01 14:03:16 +0200

    changes from upstream

--- terraform/SANBI/main.tf
+++ terraform/SANBI/main.tf
@@ -1,57 +1,49 @@
-resource "openstack_compute_instance_v2" "galaxy-masters2" {
-  name        = "galaxy-masters2"
+resource "openstack_compute_instance_v2" "irida-v20-05-galaxy" {
+  name        = "irida-v20-05-galaxy"
   flavor_name = "m1.xlarge"
-  key_pair    = "pvhebmkey"
+  key_pair    = "iridav2005gxkey"
   image_name  = "ubuntu-18.04-server"
 
   network {
-    port = openstack_networking_port_v2.m_net_1_g_m2_port.id
+    port = openstack_networking_port_v2.irida_network_1_i_g_port.id
   }
 
   network {
-    port = openstack_networking_port_v2.c-net_g_m2_port.id
+    port = openstack_networking_port_v2.c-net_i_g_port.id
   }
-
-  # provisioner "local-exec" {
-  #   command = "python ~/terraform/dns/update_dns.py -a -n ${self.name} -i ${openstack_compute_floatingip_v2.nextcloud_fip.address}"
-  # }
-
-  # provisioner "local-exec" {
-  #   command = "python ~/terraform/dns/update_dns.py -r -n ${self.name}"
-  #   when = "destroy"
-  # }  
+ 
 }
 
-output "g_m2_floatingip_address" {
-  value = openstack_compute_floatingip_associate_v2.g_m2_floatingip_associate.floating_ip
+output "i_g_floatingip_address" {
+  value = openstack_compute_floatingip_associate_v2.i_g_floatingip_associate.floating_ip
 }
 
-resource "openstack_compute_keypair_v2" "pvhebmkey" {
-  name = "pvhebmkey"
+resource "openstack_compute_keypair_v2" "iridav2005gxkey" {
+  name = "iridav2005gxkey"
 }
 
-resource "openstack_compute_floatingip_associate_v2" "g_m2_floatingip_associate" {
-  floating_ip = openstack_compute_floatingip_v2.g_m2_floatingip.address
-  instance_id = openstack_compute_instance_v2.galaxy-masters2.id
+resource "openstack_compute_floatingip_associate_v2" "i_g_floatingip_associate" {
+  floating_ip = openstack_compute_floatingip_v2.i_g_floatingip.address
+  instance_id = openstack_compute_instance_v2.irida-v20-05-galaxy.id
 }
 
-resource "openstack_blockstorage_volume_v2" "g_m2_store" {
-  name = "g_m2_store"
+resource "openstack_blockstorage_volume_v2" "i_g_store" {
+  name = "i_g_store"
   size = 160
 }
 
-resource "openstack_compute_volume_attach_v2" "g_m2_store_attach" {
-  instance_id = openstack_compute_instance_v2.galaxy-masters2.id
-  volume_id = openstack_blockstorage_volume_v2.g_m2_store.id
+resource "openstack_compute_volume_attach_v2" "i_g_store_attach" {
+  instance_id = openstack_compute_instance_v2.irida-v20-05-galaxy.id
+  volume_id = openstack_blockstorage_volume_v2.i_g_store.id
 }
 
-resource "openstack_compute_floatingip_v2" "g_m2_floatingip" {
+resource "openstack_compute_floatingip_v2" "i_g_floatingip" {
   pool = "public1"
 }
 
-resource "openstack_compute_secgroup_v2" "public_server_secgroup" {
-  name = "public_server_secgroup"
-  description = "public server security group: SSH and HTTP/S"
+resource "openstack_compute_secgroup_v2" "secgroup_galaxy" {
+  name = "secgroup_galaxy"
+  description = "galaxy server security group: SSH and HTTP/S"
 
   # rule {
   #   from_port = 0
@@ -89,26 +81,26 @@ resource "openstack_compute_secgroup_v2" "public_server_secgroup" {
   }
 }
 
-resource "openstack_networking_network_v2" "masters_net_1" {
-  name = "masters_net_1"
+resource "openstack_networking_network_v2" "irida_network_1" {
+  name = "irida_network_1"
 }
 
-resource "openstack_networking_subnet_v2" "masters_net_1_sn_1" {
-  name       = "masters_net_1_sn_1"
-  network_id = openstack_networking_network_v2.masters_net_1.id
+resource "openstack_networking_subnet_v2" "irida_network_1_sn_1" {
+  name       = "irida_network_1_sn_1"
+  network_id = openstack_networking_network_v2.irida_network_1.id
   cidr       = "192.168.50.0/24"
   allocation_pool {
     start = "192.168.50.50"
     end = "192.168.50.100"
   }
   dns_nameservers = ["192.168.2.75", "192.168.2.8"]
-  ip_version = 4
+  ip_version = 4  
 }
 
-resource "openstack_networking_port_v2" "m_net_1_g_m2_port" {
-  name               = "m_net_1_g_m2_port"
-  network_id         = openstack_networking_network_v2.masters_net_1.id
-  security_group_ids = [openstack_compute_secgroup_v2.public_server_secgroup.id]
+resource "openstack_networking_port_v2" "irida_network_1_i_g_port" {
+  name               = "irida_network_1_i_g_port"
+  network_id         = openstack_networking_network_v2.irida_network_1.id
+  security_group_ids = [openstack_compute_secgroup_v2.secgroup_galaxy.id]
   admin_state_up     = true
 }
 
@@ -125,9 +117,9 @@ resource "openstack_networking_subnet_v2" "ceph-net_sn_1" {
   ip_version = 4
 }
 
-resource "openstack_networking_port_v2" "c-net_g_m2_port" {
-  name = "c-net_g_m2_port"
+resource "openstack_networking_port_v2" "c-net_i_g_port" {
+  name = "c-net_i_g_port"
   network_id = openstack_networking_network_v2.ceph-net.id
-  security_group_ids = [openstack_compute_secgroup_v2.public_server_secgroup.id]
+  security_group_ids = [openstack_compute_secgroup_v2.secgroup_galaxy.id]
   admin_state_up = true
 }
